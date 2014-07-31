bcba7057adb4cb861f3597e01173c989ebe46936
uday <udkumar@hotmail.com>
2014-07-31 22:30:14 +0530

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Galleydemo::Application.config.secret_key_base = '16175a1756b937d5e1d19e6c3ef7eb59e292580cbc44dda8ccdaa114bbd57ac4db58c7e7df002800db413d9bd51af71eab80cc6b42161ca4b5f6decb01adf83d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Galleydemo::Application.config.session_store :cookie_store, key: '_galleydemo_session'
